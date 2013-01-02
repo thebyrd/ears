822ac8b5494b4f25cf02591621247cd25972c333
David Byrd <david@byrdhou.se>
2013-01-01 22:23:00 -1000

    Update ears.js

--- ears.js
+++ ears.js
@@ -1,19 +1,20 @@
 var Ears = klass()
-  .methods({
-    initialize: function () {
-      var ear = this;
-      var events = this.constructor.events;
-      
-      !events ?
-        console.log("no events to initialize")
-      : v(events).each(function (key) {
+    .methods({
+      initialize: function () {
+        var ear = this;
+        var events = this.constructor.events;
         
-          var e = key.split(/\s+/),
-              action = e[0],
-              selector = e.slice(1).join(' '),
-              fn = events[key];
+        !events ?
+          console.log("no events to initialize")
+        : v(events).each(function (key) {
+          
+            var e = key.split(/\s+/),
+                action = e[0],
+                selector = e.slice(1).join(' '),
+                fn = events[key];
 
-          $(selector).bind(action, function (e) { ear[fn](e); });
-        });
-    }
-  });
+            $(selector).bind(action, function (e) { ear[fn](e); });
+
+          });
+      }
+    });
